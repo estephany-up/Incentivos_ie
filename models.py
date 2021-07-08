8e86e9f3e35e7009e2c628de0302c96e16656f9e
estephany <ec.jaureguia@alum.up.edu.pe>
2021-07-07 22:14:17 -0500

    first commit

--- models.py
+++ models.py
@@ -9,27 +9,93 @@ from otree.api import (
     currency_range,
 )
 
+import random
 
-author = 'Your name here'
+author = 'Estephany y Yadira'
 
 doc = """
-Your app description
+Tareas de conteo y verbal - 4 rondas cada una
 """
-
 
 class Constants(BaseConstants):
     name_in_url = 'incentivos'
     players_per_group = None
     num_rounds = 1
-
+    task_time_v_s=180
+    task_time_v_p=120
+    task_time_c_s=300
+    task_time_c_p=180
 
 class Subsession(BaseSubsession):
     pass
 
-
 class Group(BaseGroup):
     pass
 
+def make_field(label):
+    return models.IntegerField(
+        choices=[[1,'Totalmente de acuerdo'],[2,'De acuerdo'],[3,'En desacuerdo'],[4,'Totalmente en desacuerdo']],
+        label=label,
+        widget=widgets.RadioSelect,
+    )
 
 class Player(BasePlayer):
-    pass
+    num_ID = models.StringField(label='1. ¿Cuál es tu número ID')
+    age = models.IntegerField(label='3. ¿Cuál es tu edad?', min=13, max=40)
+    gender = models.StringField(
+        choices=[[0, 'Masculino'], [1, 'Femenino']],
+        label='2. ¿Cuál es tu género?',
+        widget=widgets.RadioSelect,
+    )
+    career = models.StringField(
+        choices=[['Derecho', 'Derecho'], ['Finanzas', 'Finanzas'],['Marketing','Marketing'],
+        ['Economía','Economía'],['Contabilidad','Contabilidad'],['Administración','Administración'],
+        ['Ingeniería informática','Ingeniería informática'],['Ingeniería empresarial','Ingeniería empresarial'],
+        ['Negocios internacionales','Negocios internacionales']],
+        label='4. ¿Cuál es la carrera que estudias? Seleccione su carrera',
+        widget=widgets.RadioSelect,
+    )
+    ciclo = models.IntegerField(label='5. ¿Qué ciclo estás cursando actualmente?')
+    escala = models.StringField(label='6. ¿En qué escala de pensión te encuentras?')
+    exp = models.StringField(
+        choices=[[0,'No'],[1,'Sí, una vez'],[2,'Sí, más de una vez']],
+        label='7. ¿Has participado en un experimento anteriormente?',
+    )
+    ##enunciados de competencia
+    q1 = make_field('Me gusta competir')
+    q2 = make_field('Me gusta competir individualmente')
+    q3 = make_field('Disfruto competir contra un oponente')
+    q4 = make_field('No me gusta competir contra otras personas')
+    q5 = make_field('Competir contra otros me da satisfacción')
+    q6 = make_field('La situaciones de competencia me parecen incómodas')
+    q7 = make_field('Temo competir contra otras personas')
+    q8 = make_field('Intento evitar competir contra otras personas')
+    q9 = make_field('Trato de superar a los demás a menudo')
+    q10 = make_field('No me gustan los juegos en los que el ganador se lleva todo el premio')
+    q11 = make_field('La competencia destruye la amistad')
+    q12 = make_field('Los juegos sin un solo ganador son aburridos')
+    q13 = make_field('Usualmente no es importante para mí ser el mejor')
+    q14 = make_field('Cuando juego, me gusta llevar el conteo de la puntuación')
+    ##enunciados de incentivos para todos
+    q15 = make_field('Le doy importancia a los incentivos monetarios')
+    q16 = make_field('Los incentivos monetarios no tienen ningún valor para mí')
+    q17 = make_field('Los incentivos monetarios tienen efectos positivos en mi performance')
+    q18 = make_field('Un incentivo monetario atractivo aumentará mi motivación para trabajar más duro')
+    ##enunciados adicionales para tratameinto 1 y 3
+    q19 = make_field('Los incentivos monetarios ofrecidos coinciden con mi esfuerzo en las tareas realizadas')
+    q20 = make_field('El incentivo monetario ofrecido no está a la altura de mis expectativas')
+
+    ##PARA PROBAR QUE LA TABLA DE LA ENCUESTA FUNCIONA
+    offer_1 = models.IntegerField(widget=widgets.RadioSelect, choices=[1,2,3], label='"How much do you want to contribute?"')
+    offer_2 = models.IntegerField(widget=widgets.RadioSelect, choices=[1,2,3], label='"How much do you want to contribute?"')
+    
+
+    treatment = models.StringField()
+
+##por ahora sólo se ha asignado tratamiento por participante
+##cambiar más adelante a grupos
+def creating_session(subsession:Subsession):
+     #randomize to treatments
+        for player in subsession.get_players():
+            player.treatment = random.choice(['C', 'T1', 'T2', 'T3'])
+            print('Treatment:', player.treatment)
--- pages.py
+++ pages.py
@@ -6,13 +6,61 @@ from .models import Constants
 class MyPage(Page):
     pass
 
+class Introduccion(Page):
+    pass
+
+class Instrucciones_verbal(Page):
+    pass
+
+class Intrucciones_conteo(Page):
+    pass
+
+class Prueba_verbal(Page):
+    pass
+
+class Prueba_conteo(Page):
+    pass
+
+class Tarea_verbal(Page):
+    timer_text = 'Tiempo que le falta para completar la ronda: '
+    #usar if para poner el tiempo dependiendo del grupo de tratamiento o control
+    # timeout_seconds = Constants.task_time_v_s
+
+    #def before_next_page(self):
+    #   self.player.payoff = c(self.player.solved_tasks)
+
+class Tarea_conteo(Page):
+    timer_text = 'Tiempo que le falta para completar la ronda: '
+    #timeout_seconds = Constants.task_time_c_s
+    #def before_next_page(self):
+    #   self.player.payoff = c(self.player.solved_tasks)
+
+class Ranking_verbal(Page):
+    pass
+
+class Ranking_conteo(Page):
+    pass
+
+class Encuesta_final(Page):
+    form_model='player'
+    form_fields=['gender','age','career','ciclo','escala','exp']
 
 class ResultsWaitPage(WaitPage):
     pass
-
 
 class Results(Page):
     pass
 
 
-page_sequence = [MyPage, ResultsWaitPage, Results]
+page_sequence = [
+    MyPage,
+    Introduccion,
+    Instrucciones_verbal,
+    Tarea_verbal,
+    Ranking_verbal,
+    Intrucciones_conteo,
+    Tarea_conteo, 
+    Ranking_conteo,
+    Encuesta_final,
+    ResultsWaitPage, 
+    Results]
